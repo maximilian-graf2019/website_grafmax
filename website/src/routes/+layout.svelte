00acaa28f6dfdd9ff352d614e229d8ac9deb7829
Maximilian Graf <mail@grafmaximilian.com>
2023-07-14 22:16:14 +0200

    initial page

---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+	import github from '$lib/images/github.svg';
+	import instagram from '$lib/images/instagram.png';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<div>
+		<a href="https://github.com/maximilian-graf2019">
+			<img src={github} alt="GitHub" />
+		</a>
+		<a href="https://www.instagram.com/grafmaximilian/">
+			<img src={instagram} alt="Instagram" />
+		</a>
+		</div>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	footer img {
+		width: 50px;
+		height: 50px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Welcome on my Website
+	</h1>
+	<p>
+		Content pending...
+	</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
